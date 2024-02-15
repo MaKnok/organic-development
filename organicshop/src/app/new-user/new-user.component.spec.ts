558966e282f93456d770f59df1f5dd2de202bbe6
Marina <marinaknok@gmail.com>
2024-02-14 21:56:55 -0300

    new user test

--- organicshop/src/app/new-user/new-user.component.spec.ts
+++ organicshop/src/app/new-user/new-user.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { NewUserComponent } from './new-user.component';
 import { NewUserService } from './new-user.service';
 import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, AbstractControl, AsyncValidatorFn, ValidationErrors  } from '@angular/forms';
 import { confirmPasswordValidator } from './confirm-password.validator';
 import { UserExistsService } from './user-exists.service';
@@ -9,11 +10,12 @@ import { userPasswordEqualValidator } from './user-password-equal.validator';
 import { passwordStrenghtValidator } from './password-strength.validator';
 import { ageValidator } from './age.validator';
 import { ModalService } from '../tools/modal/modal.service';
-import { of } from 'rxjs';
+import { of, throwError } from 'rxjs';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 describe(NewUserComponent.name, () => {
   let component: NewUserComponent;
@@ -21,6 +23,9 @@ describe(NewUserComponent.name, () => {
   let newUserService: jasmine.SpyObj<NewUserService>;
   let formBuilder: FormBuilder;
   let router: Router;
+  let httpTestingController: HttpTestingController;
+
+  const API = environment.apiURL;
 
   const userExistsSpy = jasmine.createSpy('userExists').and.callFake(() => {
     return (control: AbstractControl) => {
@@ -39,10 +44,11 @@ describe(NewUserComponent.name, () => {
     emailExists: emailExistsSpy
   });
 
+  let newUserServiceSpy: jasmine.SpyObj<NewUserService>;
 
   beforeEach(async () => {
 
-    const newUserServiceSpy = jasmine.createSpyObj('NewUserService', ['registerNewUser']);
+    newUserServiceSpy = jasmine.createSpyObj('NewUserService', ['registerNewUser']);
 
     await TestBed.configureTestingModule({
       declarations: [ NewUserComponent ],
@@ -52,7 +58,7 @@ describe(NewUserComponent.name, () => {
                   { provide: NewUserService, useValue: newUserServiceSpy },
                   { provide: UserExistsService, useValue: userExistsServiceMock },
                     ],
-      imports: [ HttpClientModule,ReactiveFormsModule,RouterTestingModule ],
+      imports: [ HttpClientModule,ReactiveFormsModule,RouterTestingModule,HttpClientTestingModule ],
       schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
@@ -61,6 +67,7 @@ describe(NewUserComponent.name, () => {
     component = fixture.componentInstance;
     router = TestBed.inject(Router);
     newUserService = TestBed.inject(NewUserService) as jasmine.SpyObj<NewUserService>;
+    httpTestingController = TestBed.inject(HttpTestingController);
     formBuilder = TestBed.inject(FormBuilder);
 
     component.newUserForm = formBuilder.group({
@@ -82,12 +89,22 @@ describe(NewUserComponent.name, () => {
       //whole form validator
     });
 
+    spyOn(component, 'onModalChangeUserWasAdded').and.stub();
+    spyOn(component, 'onModalChangeThereWasAnError').and.stub();
+
     fixture.detectChanges();
   });
+
+  function getInvalidFeedbackElement(fixture: ComponentFixture<NewUserComponent>): HTMLElement {
+    return fixture.debugElement.query(By.css('div[class=invalid-feedback]')).nativeElement;
+  }
+  
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  /* General form validation */
 
   it('should disable submit button when form is invalid', () => {
     // Simulate form validation by setting an invalid form state
@@ -99,6 +116,171 @@ describe(NewUserComponent.name, () => {
     const submitButton = fixture.debugElement.query(By.css('button[type=submit]')).nativeElement;
     expect(submitButton.disabled).toBeTruthy();
   });
+
+  /* User name validation */
+
+  it('should validate if user name is filled', () => {
+    component.newUserForm.controls['userName'].setValue(''); 
+    component.newUserForm.controls['userName'].markAsTouched(); 
+
+    fixture.detectChanges();
+
+    const invalidFeedback = getInvalidFeedbackElement(fixture);
+
+    const feedback = invalidFeedback.textContent.trim() == 'Nome do usuário obrigatório';
+    expect(feedback).toBeTrue();
+  })
+
+  /* User email validation */
+
+  it('should validate if email field is filled', () => {
+    component.newUserForm.controls['userEmail'].setValue('');
+    component.newUserForm.controls['userEmail'].markAsTouched();
+
+    fixture.detectChanges();
+
+    const invalidFeedback = getInvalidFeedbackElement(fixture);
+    const feedback = invalidFeedback.textContent.trim() == 'E-mail obrigatório';
+    expect(feedback).toBeTrue();
+  })
+
+  it('should validate if email is valid', () => {
+    component.newUserForm.controls['userEmail'].setValue('xyz.com');
+    component.newUserForm.controls['userEmail'].markAsTouched();
+
+    fixture.detectChanges();
+
+    const invalidFeedback = getInvalidFeedbackElement(fixture);
+    const feedback = invalidFeedback.textContent.trim() == 'E-mail no formato incorreto';
+    expect(feedback).toBeTrue();
+
+  })
+
+  /* User password validation */
+
+  it('should validate if password field is filled', () => {
+    component.newUserForm.controls['userPassword'].setValue('');
+    component.newUserForm.controls['userPassword'].markAsTouched();
+
+    fixture.detectChanges();
+
+    const invalidFeedback = getInvalidFeedbackElement(fixture);
+    const feedback = invalidFeedback.textContent.trim() == 'Senha obrigatória';
+    expect(feedback).toBeTrue();
+  })
+
+  it('should validate if password is different from username', async () => {
+
+    component.newUserForm.controls['userName'].setAsyncValidators(null);
+
+    component.newUserForm.controls['userName'].setValue('Test');
+    component.newUserForm.controls['userPassword'].setValue('Test');
+
+    fixture.detectChanges();
+
+    const invalidFeedback = getInvalidFeedbackElement(fixture);
+    const feedback = invalidFeedback.textContent.trim() == 'Senha deve ser diferente do usuário';
+    expect(feedback).toBeTrue();
+  })
+
+  it('should validate if password is safe enough', () => {
+    component.newUserForm.controls['userPassword'].setValue('Test');
+
+    fixture.detectChanges();
+
+    const invalidFeedback = getInvalidFeedbackElement(fixture);
+    const feedback = invalidFeedback.textContent.trim() == 'Senha deve ter pelo menos 8 caracteres, um número e um caracter especial (Exemplo: VbX9#aweRt!)';
+    expect(feedback).toBeTrue();
+  })
+
+  it('should validate if password and password confirmation match', () => {
+    component.newUserForm.controls['userPassword'].setValue('Test#123');
+    component.newUserForm.controls['userPasswordConfirm'].setValue('Test#124');
+
+    fixture.detectChanges();
+
+    const invalidFeedback = getInvalidFeedbackElement(fixture);
+    const feedback = invalidFeedback.textContent.trim() == 'A confirmação da senha deve ter o mesmo valor do campo senha';
+    expect(feedback).toBeTrue();
+  })
+
+  /* User fullname validation */
+
+  it('should validate if fullname field is filled', () => {
+    component.newUserForm.controls['userFullName'].setValue('');
+    component.newUserForm.controls['userFullName'].markAsTouched();
+
+    fixture.detectChanges();
+
+    const invalidFeedback = getInvalidFeedbackElement(fixture);
+    const feedback = invalidFeedback.textContent.trim() == 'Nome completo obrigatório';
+    expect(feedback).toBeTrue();
+  })
+
+  /* User birthday validation */
+
+  it('should validate if birthday field is filled', () => {
+    component.newUserForm.controls['userBirthday'].setValue('');
+    component.newUserForm.controls['userBirthday'].markAsTouched();
+
+    fixture.detectChanges();
+
+    const invalidFeedback = getInvalidFeedbackElement(fixture);
+    const feedback = invalidFeedback.textContent.trim() == 'Data de nascimento obrigatória';
+    expect(feedback).toBeTrue();
+  })
+
+  it('should validate if user is not under 18', () => {
+    const under18Date = new Date();
+    under18Date.setFullYear(under18Date.getFullYear() - 17);
+    component.newUserForm.controls['userBirthday'].setValue(under18Date);
+
+    fixture.detectChanges();
+
+    const invalidFeedback = getInvalidFeedbackElement(fixture);
+    const feedback = invalidFeedback.textContent.trim() == 'Você precisa ter 18 anos ou mais';
+    expect(feedback).toBeTrue();
+  })
+
+  it('should validate if date is not future date', () => {
+    const futureDate = new Date();
+    futureDate.setFullYear(futureDate.getFullYear() + 1);
+    component.newUserForm.controls['userBirthday'].setValue(futureDate);
+
+    fixture.detectChanges();
+
+    const invalidFeedback = getInvalidFeedbackElement(fixture);
+    const feedback = invalidFeedback.textContent.trim() == 'Data de nascimento inválida';
+    expect(feedback).toBeTrue();
+  })
+
+  /* User segment validation */
+
+  it('should validate if segment field is filled', () => {
+    component.newUserForm.controls['userSegment'].setValue('');
+    component.newUserForm.controls['userSegment'].markAsTouched();
+
+    fixture.detectChanges();
+
+    const invalidFeedback = getInvalidFeedbackElement(fixture);
+    const feedback = invalidFeedback.textContent.trim() == 'Segmento do usuário obrigatório';
+    expect(feedback).toBeTrue();
+  })
+
+  /* User role validation */
+
+  it('should validate if role field is filled', () => {
+    component.newUserForm.controls['userRole'].setValue('');
+    component.newUserForm.controls['userRole'].markAsTouched();
+
+    fixture.detectChanges();
+
+    const invalidFeedback = getInvalidFeedbackElement(fixture);
+    const feedback = invalidFeedback.textContent.trim() == 'Cargo do novo usuário obrigatório';
+    expect(feedback).toBeTrue();
+  })
+
+  /** End form items */
 
 
   it('should clear the form when the clear button is clicked', () => {
@@ -187,8 +369,43 @@ describe(NewUserComponent.name, () => {
       userRole: 'Desenvolvedor Fullstack'
     }));
 
+    expect(component.onModalChangeUserWasAdded).toHaveBeenCalledWith(true);
 
-  })
+
+  });
+
+  it('should call onModalChangeThereWasAnError when error comes from the server', async () => {
+    // Mock an error response from the service
+
+    component.newUserForm.controls['userName'].setAsyncValidators(null);
+    component.newUserForm.controls['userEmail'].setAsyncValidators(null);
+
+    component.newUserForm.setValue({
+      userName: 'Test User Name',
+      userEmail: 'testemail@test.com',
+      userPassword: '123#Test',
+      userPasswordConfirm: '123#Test',
+      userFullName: 'User Test Name',
+      userBirthday: '1990-01-01',
+      userSegment: 'TI',
+      userRole: 'Desenvolvedor Fullstack'
+    }, 
+    { emitEvent: false }
+    );
+    
+    const errorResponse = { status: 500, message: 'Internal Server Error' };
+    newUserService.registerNewUser.and.returnValue(throwError(errorResponse));
+  
+    // Trigger the method
+    component.registerNewUser();
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+  
+    // Expect onModalChangeThereWasAnError to have been called with true
+    expect(component.onModalChangeThereWasAnError).toHaveBeenCalledWith(true);
+  });
+
 
   it('should navigate to home page when the router link is clicked', () => {
     const navigateSpy = spyOn(router, 'navigateByUrl').and.stub();
